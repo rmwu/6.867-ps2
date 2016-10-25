1310fcd6b426d8eeaa89588a2f1c9f46a965adcf
Tony Zhang <txz@mit.edu>
2016-10-24 23:05:21 -0400

    made linear kernel simpler

--- code/svm.py
+++ code/svm.py
@@ -2,9 +2,6 @@ import math
 import numpy as np
 import cvxopt
 
-def elementwise_kernel(kernel, vectors, point):
-    return np.array([kernel(vector, point) for vector in vectors])
-
 def train_svm(X, Y, C=float('inf'),
               kernel_func=None,
               kernel_mat=None,
@@ -111,7 +108,7 @@ def optimal_weight_vector(X, Y, alphas):
 def linear_kernel(z1, z2):
     # TODO: support arbitrary shapes for z1, z2
     # as long as they have the same final dimension
-    return 1 + z1.dot(z2.T)
+    return z1.dot(z2.T)
 
 def make_gaussian_rbf(bandwidth):
     """
